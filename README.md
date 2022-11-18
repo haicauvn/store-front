1f4683c7fc706d9d62825e8f91fb190df63119bb
haicauvn <haidv245@gmail.com>
2022-11-18 18:10:35 +0700

    init project

---
+++ README.md
@@ -0,0 +1,5 @@
+db-migrate create user-table --sql-file
+
+db-migrate up
+
+db-migrate down
---
+++ migrations/sqls/20221118105941-user-table-up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY  KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    user_name VARCHAR(100),
+    password VARCHAR(200)
+);
